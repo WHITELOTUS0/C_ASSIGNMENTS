c4698c4c7695fa9f2c109a7dbcc26b5321033ee2
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-19 21:01:15 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+
+int main(){
+   int i,j;
+   printf("Number\tNumber\tProduct\n");
+   for (i=0; i<10;i++){
+    for(j=0; j<10;j++){
+        
+        printf("%d\t%d\t%d\n", i, j, (i*j));
+
+    }
+    printf("\n");
+    printf("===================\n\n");
+   printf("Number\tNumber\tProduct\n");
+
+   }
+   
+   return 0;
+  
+    
+
+}
