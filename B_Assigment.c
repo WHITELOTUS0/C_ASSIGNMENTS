7e6b31b358fac2ad2171ccdcf2d7d3c41117ca1c
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-01-28 23:40:09 +0300

    Update B_Assigment.c

--- B_Assigment.c
+++ B_Assigment.c
@@ -6,7 +6,7 @@ int main()
     char sNo[30]="2000722109";
     char phoneNo[40]="0758541682";
     printf("My name is %s\n", Name);
-    printf("My student number is %s\n", sNo);
+    printf("My student number %s\n", sNo);
     printf("My phone number is %s\n", phoneNo);
     return 0;
 }
